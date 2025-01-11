82b5ca54f3251488abf2e1225d6b433c74fe4257
Loay Sherif <143604561+Loaysherif@users.noreply.github.com>
2025-01-11 13:58:39 +0200

    Dark mode
    Added darkmode functionality

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -2,23 +2,52 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:tourist_guide/nav_bar.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = false;
+
+  void _toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Egypt Tourist Guide',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal.shade900),
-        useMaterial3: true,
-      ),
+      theme: _isDarkMode
+          ? ThemeData.dark().copyWith(
+              colorScheme:
+                  ColorScheme.fromSeed(seedColor: Colors.teal.shade900),
+            )
+          : ThemeData(
+              colorScheme:
+                  ColorScheme.fromSeed(seedColor: Colors.teal.shade900),
+              useMaterial3: true,
+            ),
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
-      home: const MyNavigationBar(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Egypt Tourist Guide'),
+          actions: [
+            IconButton(
+              icon: Icon(_isDarkMode ? Icons.dark_mode : Icons.light_mode),
+              onPressed: _toggleTheme,
+            ),
+          ],
+        ),
+        body: const MyNavigationBar(),
+      ),
     );
   }
 }
