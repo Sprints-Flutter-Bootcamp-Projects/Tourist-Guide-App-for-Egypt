a49d57851ecabfd5596acb8a3ed9d7ee34d2a4a5
MoSwidan <magdy8912@gmail.com>
2025-01-25 23:59:21 +0200

    fix issues

--- lib/views/profile/profile.dart
+++ lib/views/profile/profile.dart
@@ -1,12 +1,11 @@
-//customizalbe flutter moji
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tourist_guide/blocs/authentication/auth_bloc.dart';
+import 'package:tourist_guide/helpers/shared_pref.dart';
 import 'package:tourist_guide/navigation/app_drawer.dart';
 import 'package:tourist_guide/views/authentication/login_page.dart';
 import 'package:fluttermoji/fluttermoji.dart';
-
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -19,17 +18,22 @@ class _ProfileState extends State<Profile> {
   bool isUser = false;
   Map<String, dynamic>? userData;
 
+  @override
+  void initState() {
+    super.initState();
+    checkUser();
+  }
+
   void checkUser() async {
     userData = await SharedPreferencesHelper.getUserData();
     if (userData != null) {
-      // print('Profile User data: $userData');
       isUser = true;
     } else {
-      // print("No user data found.");
       isUser = false;
     }
     setState(() {});
   }
+
   String hashedPassword(String password) {
     String hashed = '';
     for (int i = 0; i < password.length; i++) {
@@ -40,200 +44,155 @@ class _ProfileState extends State<Profile> {
 
   @override
   Widget build(BuildContext context) {
-    checkUser();
     return Scaffold(
-        appBar: AppBar(title: Text(tr("profile"))),
-        drawer: const AppDrawer(),
-        body: BlocBuilder<AuthBloc, AuthState>(
-          builder: (context, state) {
-            if (state is AuthAuthenticated) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Center(
-                        child: Icon(
-                          Icons.person_rounded,
-                          size: 150,
-                          color: Colors.teal[900],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: SizedBox(
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  context.tr("name"),
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                      color: Colors.teal,
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  state.userData['name'],
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                    fontSize: 17,
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: SizedBox(
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  context.tr("email"),
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                      color: Colors.teal,
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  state.userData['email'],
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(fontSize: 13),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  context.tr("password"),
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                      color: Colors.teal,
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  hashedPassword(state.userData['password']),
-                                  style: const TextStyle(fontSize: 17),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            } else {
-              return Center(
+      appBar: AppBar(title: Text(tr("profile"))),
+      drawer: const AppDrawer(),
+      body: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          if (state is AuthAuthenticated) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Center(
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => FluttermojiCustomizer(),
-                            ),
-                          );
-                        },
-                        child: FluttermojiCircleAvatar(
-                          radius: 75,
-                          backgroundColor: Colors.teal[100],
+                      child: Icon(
+                        Icons.person_rounded,
+                        size: 150,
+                        color: Colors.teal[900],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: SizedBox(
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                context.tr("name"),
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                    color: Colors.teal,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                state.userData['name'],
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 17,
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                    Text(
-                      context.tr("no_profile_desc"),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: SizedBox(
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                context.tr("email"),
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                    color: Colors.teal,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                state.userData['email'],
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(fontSize: 13),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    FilledButton(
-                      style: FilledButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(6))),
-                      onPressed: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const LoginPage(),
-                          )),
-                      child: Text(
-                        tr('log_in'),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                context.tr("password"),
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                    color: Colors.teal,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                hashedPassword(state.userData['password']),
+                                style: const TextStyle(fontSize: 17),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
                     ),
                   ],
                 ),
-              );
-            }
-          },
-        ));
               ),
-            )
-          : Center(
+            );
+          } else {
+            return Center(
               child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // FluttermojiCircleAvatar wrapped in InkWell
-                Center(
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => FluttermojiCustomizer(),
-                        ),
-                      );
-                    },
-                    child: FluttermojiCircleAvatar(
-                      radius: 75,
-                      backgroundColor: Colors.teal[100],
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => FluttermojiCustomizer(),
+                          ),
+                        );
+                      },
+                      child: FluttermojiCircleAvatar(
+                        radius: 75,
+                        backgroundColor: Colors.teal[100],
+                      ),
                     ),
                   ),
-                ),
-                Text(
-                  context.tr("no_profile_desc"),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                FilledButton(
-                  style: FilledButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6))),
-                  onPressed: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginPage(),
-                      )),
-                  child: Text(
-                    tr('log_in'),
+                  Text(
+                    context.tr("no_profile_desc"),
                   ),
-                ),
-              ],
-            )),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  FilledButton(
+                    style: FilledButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6))),
+                    onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        )),
+                    child: Text(
+                      tr('log_in'),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
