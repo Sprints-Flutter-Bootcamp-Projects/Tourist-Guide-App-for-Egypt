d815586e1662cfe6310da2b378947200c8305c8c
MohamedAlyMatar <mohamedalymatar2001@gmail.com>
2025-02-08 02:51:12 +0200

    added a button in the landmark map for the user to start navigation from his position

--- lib/views/government/landmark_map.dart
+++ lib/views/government/landmark_map.dart
@@ -3,16 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LandmarkMap extends StatefulWidget {
   final String locationName;
   final double latitude;
   final double longitude;
-  const LandmarkMap(
-      {super.key,
-      required this.locationName,
-      required this.latitude,
-      required this.longitude});
+
+  const LandmarkMap({
+    super.key,
+    required this.locationName,
+    required this.latitude,
+    required this.longitude,
+  });
 
   @override
   State<LandmarkMap> createState() => _LandmarkMapState();
@@ -30,7 +33,10 @@ class _LandmarkMapState extends State<LandmarkMap> {
   Future<void> _requestLocationPermission() async {
     var status = await Permission.location.request();
     if (status.isDenied) {
-      // Show an alert to the user or handle it accordingly
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text('Location permission is required for navigation')),
+      );
     }
   }
 
@@ -40,32 +46,101 @@ class _LandmarkMapState extends State<LandmarkMap> {
     });
   }
 
+  Future<void> _startNavigation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Please enable location services'),
+        ),
+      );
+      return;
+    }
+
+    // Check and request location permission
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Location permission denied')),
+        );
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Location permission permanently denied')),
+      );
+      return;
+    }
+
+    // Get the current location
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    // Construct the Google Maps navigation URL
+    Uri uri = Uri.parse(
+        'https://www.google.com/maps/dir/?api=1&origin=${position.latitude},${position.longitude}&destination=${widget.latitude},${widget.longitude}&travelmode=driving');
+
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $uri';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(context.tr(widget.locationName))),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(widget.latitude, widget.longitude),
-          zoom: 14.0,
-        ),
-        markers: {
-          Marker(
-            markerId: MarkerId('landmark'),
-            position: LatLng(widget.latitude, widget.longitude),
-            infoWindow: InfoWindow(title: 'Landmark'),
-            onTap: () async {
-              Uri uri = Uri.parse(
-                  'https://www.google.com/maps/dir/?api=1&destination=${widget.latitude},${widget.longitude}');
-              if (await canLaunchUrl(uri)) {
-                await launchUrl(uri);
-              } else {
-                throw 'Could not launch $uri';
-              }
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: LatLng(widget.latitude, widget.longitude),
+              zoom: 14.0,
+            ),
+            markers: {
+              Marker(
+                markerId: MarkerId('landmark'),
+                position: LatLng(widget.latitude, widget.longitude),
+                infoWindow: InfoWindow(title: 'Landmark'),
+              ),
             },
           ),
-        },
+          Positioned(
+            bottom: 20,
+            left: 20,
+            right: 20,
+            child: Center(
+              child: IntrinsicWidth(
+                child: ElevatedButton.icon(
+                  onPressed: _startNavigation,
+                  icon: Icon(Icons.navigation, color: Colors.redAccent),
+                  label: Text(
+                    'Start Navigation',
+                    style: TextStyle(color: Colors.blueAccent),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                    textStyle: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
